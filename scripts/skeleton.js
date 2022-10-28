fa97bae5d8c8f612de04374e95c4902ccb9f0c78
Coppertalon <brogers42@my.bcit.ca>
2022-10-28 10:56:18 -0700

    added style guide
    added visual style to grocery list

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,7 +2,7 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
+function loadSkeleton() {
     console.log($('#navbarPlaceholder').load('./text/nav.html'));
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
