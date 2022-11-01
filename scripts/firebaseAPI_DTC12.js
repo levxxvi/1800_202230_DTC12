53cf5fc4339e2563cd77edebfd4778a9bbf8513a
Alexandra Wong <lex115wong@gmail.com>
2022-11-01 16:16:54 -0700

    Push Firebase Key for everyone

--- scripts/firebaseAPI_DTC12.js
+++ scripts/firebaseAPI_DTC12.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBZBr4gqWFNiT4gDwjYFoCFDFgK0tYzOQs",
+    authDomain: "dtc12-1800projects.firebaseapp.com",
+    projectId: "dtc12-1800projects",
+    storageBucket: "dtc12-1800projects.appspot.com",
+    messagingSenderId: "658051082773",
+    appId: "1:658051082773:web:9755ddef80ebc100f6fc1c"
 };
 
 //--------------------------------------------
