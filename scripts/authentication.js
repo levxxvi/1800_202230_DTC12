3be59c7264b96bcae0f5e755f7e50cebb33aed43
Alexandra Wong <lex115wong@gmail.com>
2022-11-01 23:10:27 -0700

    Complete login

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -17,10 +17,10 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: "main.html",
+  signInSuccessUrl: 'recipes.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
