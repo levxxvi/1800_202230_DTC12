7e83fa1307af08a27354ff890b50a1a03ae079a4
Coppertalon <brogers42@my.bcit.ca>
2022-11-03 10:45:30 -0700

    added most of functionality for recipies

--- scripts/add_function.js
+++ scripts/add_function.js
@@ -4,8 +4,51 @@ function call_cancel() {
 }
 
 function call_add() {
-    $('#remove').remove();
+
+    let a = jQuery("#recipe_name").val();
+    let b = jQuery("#breakfast:checked").val();
+
+    if (b == "option1") {
+        b = "breakfast"
+    } else if (b != "breakfast") {
+        b = "not_breakfast"
+    }
+    let c = jQuery("#lunch:checked").val();
+    if (c == "option1") {
+        c = "lunch"
+    } else if (c != "lunch") {
+        c = "not_lunch"
+    }
+    let d = jQuery("#snack:checked").val();
+    if (d == "option1") {
+        d = "snack"
+    } else if (d != "snack") {
+        d = "not_snack"
+    }
+    let e = jQuery("#dinner:checked").val();
+    if (e == "option1") {
+        e = "dinner"
+    } else if (e != "dinner") {
+        e = "not_dinner"
+    }
+
+    let f = jQuery("#recipe_details").val();
+    let g = jQuery("#recipe_link").val();
+
+    if (a == "" || (b != "breakfast" && c != "lunch" && d != "snack" && e != "dinner") || (f == "")) {
+        console.log("invalid entry")
+        jQuery("#error").html("Error: one or more required fields is missing")
+        let doc = [a, b, c, d, e, f, g,]
+        console.log(doc)
+
+    } else {
+        let doc = [a, b, c, d, e, f, g,]
+        console.log(doc)
+        $('#remove').remove();
+    }
 }
+
+
 
 
 setup = function () {
