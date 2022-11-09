1751a6d485207926baa5a7555d31a3c8d42ec767
Alexandra Wong <lex115wong@gmail.com>
2022-11-09 14:16:21 -0800

    Redirects to homepage after sign in success

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -22,7 +22,7 @@ var uiConfig = {
           email: user.email,                         //with authenticated user's ID (user.uid)
         }).then(function () {
           console.log("New user added to firestore");
-          window.location.assign("main.html");       //re-direct to main.html after signup
+          window.location.assign("index.html");       //re-direct to main.html after signup
         })
           .catch(function (error) {
             console.log("Error adding new user: " + error);
@@ -40,7 +40,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'main-page.html',
+  signInSuccessUrl: 'home.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
