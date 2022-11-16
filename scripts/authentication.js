5aa41b426568c65af0c1fac69c9856ddb2dc99a5
Alexandra Wong <lex115wong@gmail.com>
2022-11-16 11:59:48 -0800

    Failed add subcollection

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -27,7 +27,9 @@ var uiConfig = {
           .catch(function (error) {
             console.log("Error adding new user: " + error);
           });
-        var userRef = db.collection(user.displayName + "_recipes");
+        db.collection("user").doc(user.uid).collection(user.uid+"Recipes");
+        db.collection("user").doc(user.uid).collection(user.uid+"Schedule");
+
       } else {
         return true;
       }
@@ -41,7 +43,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'home.html',
+  signInSuccessUrl: 'HTML/home copy.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
