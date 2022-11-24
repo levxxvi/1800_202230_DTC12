515abcce7d1bf74214f8d0e63811f40877f8ebbb
Alexandra Wong <lex115wong@gmail.com>
2022-11-24 13:25:12 -0800

    Push code

--- scripts/home.js
+++ scripts/home.js
@@ -51,36 +51,36 @@ function populateCardsDynamically() {
     db.collection("users").doc(uUid).collection(uDisplayName + "Schedule").doc(today).get().then((doc) => {
         console.log(uUid, uDisplayName);
         console.log(doc.id, " => ", doc.data());
-        // card_num += 1
-        // $("#todaysMeals").append(
-        //     `
-        // <button class = "recipe_card" id = "${doc.data().name}">
-        //     <div class="cards" id="top_card">
-        //         <img src="https://picsum.photos/${photonum}">
-        //         <div id="text_area">
-        //             <div id="title_area">
-        //                 <h1 class="card_title" id="get_title_3">
-        //                     ${doc.data().name}
-        //                 </h1>
-        //                 <hr>
-        //             </div>
+        card_num += 1
+        $("#todaysMeals").append(
+            `
+        <button class = "recipe_card" id = "${doc.data().name}">
+            <div class="cards" id="top_card">
+                <img src="https://picsum.photos/${photonum}">
+                <div id="text_area">
+                    <div id="title_area">
+                        <h1 class="card_title" id="get_title_3">
+                            ${doc.data().name}
+                        </h1>
+                        <hr>
+                    </div>
 
-        //             <div id="list_area">
-        //                 <ul class="lists" id="card_list_${card_num}">
+                    <div id="list_area">
+                        <ul class="lists" id="card_list_${card_num}">
 
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </button>
-        // `
-        // )
-        // photonum += 1
-        // let list = doc.data().tag
-        // let list_len = list.length
-        // for (let i = 0; i < list_len; i++) {
-        //     $(`#card_list_${card_num}`).append('<li class="list_item">' + list[i] + '</li>');
-        // }
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </button>
+        `
+        )
+        photonum += 1
+        let list = doc.data().tag
+        let list_len = list.length
+        for (let i = 0; i < list_len; i++) {
+            $(`#card_list_${card_num}`).append('<li class="list_item">' + list[i] + '</li>');
+        }
 
     });
     return null;
