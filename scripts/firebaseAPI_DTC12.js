edc0d88bb2e10f21e2f8a149e401beb5cfd0a42d
mimikwkk <113390005+mimikwkk@users.noreply.github.com>
2022-11-25 10:42:35 -0800

    document code

--- scripts/firebaseAPI_DTC12.js
+++ scripts/firebaseAPI_DTC12.js
@@ -1,5 +1,5 @@
 //----------------------------------------
-//  Your web app's Firebase configuration
+//  Our web app's Firebase configuration
 //----------------------------------------
 const firebaseConfig = {
     apiKey: "AIzaSyDtWqfkg1cVFvOK2jwF0M8Uk5sxzrqy-nM",
