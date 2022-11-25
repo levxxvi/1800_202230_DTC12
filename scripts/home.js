9fb5043482026b63aba2de24885d5950a56d7e69
Alexandra Wong <lex115wong@gmail.com>
2022-11-25 11:55:06 -0800

    Unhardcode date

--- scripts/home.js
+++ scripts/home.js
@@ -5,50 +5,22 @@ var today;
 
 
 function populateDate() {
-    // today = new Date();
-    // var dd = String(today.getDate()).padStart(2, '0');
-    // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    // var yyyy = today.getFullYear();
+    today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
 
-    // today = yyyy + '-' + mm + '-' + dd;
-
-    today = "2022-11-23";
+    today = yyyy + '-' + mm + '-' + dd;
 
     h1.textContent = today;
 }
 populateDate();
 
 function populateCardsDynamically() {
-    // let mealCardTemplate = document.getElementById("mealCardTemplate");
-    // let mealCardGroup = document.getElementById("mealCardGroup");
-    // db.collection("users").doc(user.uid + "/" + user.displayName + "Schedule").get().then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //         // doc.data() is never undefined for query doc snapshots
-    //         console.log(doc.id, " => ", doc.data());
-    //     });
-    // })
-
-    // db.collection("added_recipes").get()
-    //     .then(allMeals => {
-    //         allMeals.forEach(doc => {
-    //             var mealName = doc.data().name; //gets the name field
-    //             //var mealID = doc.data().code; //gets the unique ID field
-    //             var mealIngredients = doc.data().length; //gets the length field
-    //             let testMealCard = hikeCardTemplate.content.cloneNode(true);
-    //             testMealCard.querySelector('.card-title').innerHTML = mealName; //equiv getElementByClassName
-    //             testMealCard.querySelector('.card-ingredients').innerHTML = mealIngredients; //equiv getElementByClassName
-
-    //             // testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);//equiv getElementByTagName
-    //             // testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;   //equiv getElementByTagName
-    //             hikeCardGroup.appendChild(testHikeCard);
-    //         })
-
-    //     })
-
     db.collection("users").doc(uUid).collection(uDisplayName + "Schedule").doc(today).get().then((doc) => {
-            console.log(uUid, uDisplayName);
-            console.log(doc.id, " => ", doc.data());
-            $("#todaysMeals").append(
+        console.log(uUid, uDisplayName);
+        console.log(doc.id, " => ", doc.data());
+        $("#todaysMeals").append(
                 `
             <h2>Breakfast</h2>
             <button class = "recipe_card" id = "${doc.data().breakfast}">
@@ -71,18 +43,9 @@ function populateCardsDynamically() {
                 </div>
             </button>
             `
-            )
-            // breakfast_meal = doc.data().breakfast
-            // db.collection("users").doc(uUid).collection(uDisplayName + "Recipes").doc(breakfast_meal).get().then((doc) => {
-            //         let list = doc.data().tag
-            //         let list_len = list.length
-            //         for (let i = 0; i < list_len; i++) {
-            //             $(`#card_list_${card_num}`).append('<li class="list_item">' + list[i] + '</li>');
-            //         }
-            //     }
-            ,
-                $("#todaysMeals").append(
-                    `
+            ),
+            $("#todaysMeals").append(
+                `
                     <h2>Lunch</h2>
         <button class = "recipe_card" id = "${doc.data().lunch}">
             <div class="cards" id="top_card">
@@ -104,8 +67,8 @@ function populateCardsDynamically() {
             </div>
         </button>
         `
-                ), $("#todaysMeals").append(
-                    `
+            ), $("#todaysMeals").append(
+                `
                     <h2>Snack</h2>
         <button class = "recipe_card" id = "${doc.data().snack}">
             <div class="cards" id="top_card">
@@ -127,8 +90,8 @@ function populateCardsDynamically() {
             </div>
         </button>
         `
-                ), $("#todaysMeals").append(
-                    `
+            ), $("#todaysMeals").append(
+                `
                     <h2>Dinner</h2>
         <button class = "recipe_card" id = "${doc.data().dinner}">
             <div class="cards" id="top_card">
@@ -150,15 +113,8 @@ function populateCardsDynamically() {
             </div>
         </button>
         `
-                )
-                // photonum += 1
-                // let list = doc.data().tag
-                // let list_len = list.length
-                // for (let i = 0; i < list_len; i++) {
-                //     $(`#card_list_${card_num}`).append('<li class="list_item">' + list[i] + '</li>');
-                // }
-
-            });
-        return null;
-    }
-    populateCardsDynamically();
+            )
+    });
+    return null;
+}
+populateCardsDynamically();
