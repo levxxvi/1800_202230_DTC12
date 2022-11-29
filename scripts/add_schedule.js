a250cace8f3056bca778660103591a8ca8fa6992
Coppertalon <brogers42@my.bcit.ca>
2022-11-29 11:44:32 -0800

    fixed schedule issue

--- scripts/add_schedule.js
+++ scripts/add_schedule.js
@@ -1,27 +1,29 @@
-    const uUid = localStorage.getItem('userUid')
-    const uDisplayName = localStorage.getItem('userDisplayName')
+const uUid = localStorage.getItem('userUid')
+const uDisplayName = localStorage.getItem('userDisplayName')
 
-    function addSchedule() {
-        //inside add schedule
-        let Date = document.getElementById("date").value;
-        let Breakfast = document.getElementById("breakfast").value;
-        let Lunch = document.getElementById("lunch").value;
-        let Snack = document.getElementById("snack").value;
-        let Dinner = document.getElementById("dinner").value;
+function addSchedule() {
+    //inside add schedule
+    let Date = document.getElementById("date").value;
+    let Breakfast = document.getElementById("breakfast").value;
+    let Lunch = document.getElementById("lunch").value;
+    let Snack = document.getElementById("snack").value;
+    let Dinner = document.getElementById("dinner").value;
 
-        db.collection("users").doc(uUid).collection(uDisplayName + "Schedule").doc(Date).set({
-            date: Date,
-            breakfast: Breakfast,
-            lunch: Lunch,
-            snack: Snack,
-            dinner: Dinner
-        }).then(() => {
-            console.log("Document successfully written!");
-            window.location.href = "../HTML/home.html";
-        })
-    }
+    db.collection("users").doc(uUid).collection(uDisplayName + "Schedule").doc(Date).set({
+        date: Date,
+        breakfast: Breakfast,
+        lunch: Lunch,
+        snack: Snack,
+        dinner: Dinner
+    }).then(() => {
+        console.log("Document successfully written!");
+        window.location.href = "../HTML/home.html";
+    })
+}
 
-    setup = function () {
-        console.log(uUid, uDisplayName);
-        jQuery("#submitBtn").click(addSchedule);
-    }
+setup = function () {
+    console.log(uUid, uDisplayName);
+    jQuery("#submitBtn").click(addSchedule);
+}
+
+$(document).ready(setup)
